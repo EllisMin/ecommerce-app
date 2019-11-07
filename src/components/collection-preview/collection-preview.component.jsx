90df9f8f67a1b4a84d4df78283837c190fcaf196
Ellis Min <dbmin@hotmail.com>
2019-11-07 04:31:25 -0600

    create shop page with collection preview and collection item components

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
+
+const howManyToShow = 4;
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, i) => i < howManyToShow)
+        .map(({ id, ...otherProps }) => (
+          <CollectionItem key={id} {...otherProps} />
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
